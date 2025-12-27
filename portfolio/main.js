6f0cf0c153a453060f3edb829edba73cdc0dc21a
KSwathi1231 <kancharlaswathi2005@gmail.com>
2025-12-27 10:46:42 +0530

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,22 @@
+
+function sendWhatsAppMessage(event) {
+  event.preventDefault();
+
+  const fname = document.getElementById("fname").value;
+  const lname = document.getElementById("lname").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  const phoneNumber = "919391056536"; // your number with country code
+
+  const text =
+    `Name: ${fname} ${lname}%0A` +
+    `Email: ${email}%0A` +
+    `Subject: ${subject}%0A` +
+    `Message: ${message}`;
+
+  const whatsappURL = `https://wa.me/${phoneNumber}?text=${text}`;
+
+  window.open(whatsappURL, "_blank");
+}
